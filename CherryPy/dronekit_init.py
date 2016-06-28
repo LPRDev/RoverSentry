716e7cb1b95f0d8d49aa7b9fcd8d1a25736fc313
petercr19 <petercr19@gmail.com>
2016-06-27 21:40:14 -0400

    added class for APM 2.6

--- CherryPy/dronekit_init.py
+++ CherryPy/dronekit_init.py
@@ -1,66 +1,89 @@
 # Version 1.0
 
-from dronekit import connect, VehicleMode
-#from droneapi import connect
-import time
-import string
-import cherrypy
-import usb
-
-# Check for APM over usb port, if not present then wait for it to connection...
-APM_usb_VendorID=0x2341
-APM_usb_ProductID=0x0010
-APM_found = False
-
-while (APM_found==False):
-	busses = usb.busses()
-	for bus in busses:
-		devices = bus.devices
-		for dev in devices:
-			if ((dev.idVendor==APM_usb_VendorID)and(dev.idProduct==APM_usb_ProductID)):
-				APM_found = True
-				print "APM Found!"
-				break	
-	if (APM_found==False):
-		print " APM connection failure: Is the the APM USB connected to Raspberry Pi?"
-	time.sleep(5)				
-				
-print "Connecting to APM module..."
-# Now that we found it, connect to it
-vehicle = connect('/dev/ttyACM0',wait_ready=True)
-speed=0.5;
-
+from dronekit import connect, VehicleMode, LocationGlobal
+import time, string, cherrypy, usb, socket, sys
 
 # Enable web server to be availble on all interfaces
 cherrypy.config.update({'server.socket_host': '0.0.0.0'})
 #move port to 9000 so it doesnt conflict with the video stream (UV4l)
 cherrypy.config.update({'server.socket_port': 9000})
 
-
-
-# Don't try anything until apm is ready
-#vehicle.armed = True  
-while not vehicle.armed:
-        print " Waiting for vehicle to initialise..."
-        time.sleep(1)
-
-### Callbacks ###
-#def groundspeed_callback(self, attr_name,value):
-	#print "Ground Speed: %s" % self.groundspeed;
-#vehicle.add_attribute_listener('groundspeed',groundspeed_callback);
-
-#def location_callback(self, attr_name, value):
-#	print "Location: %s" % self.location.global_frame;
-#vehicle.add_attribute_listener('location.global_frame',location_callback);
-
-# Battery Monitoring ....
-# Allow both voltage only battery monitoring
-vehicle.parameters['BATT_MONITOR']=4;
-
-# add callback to see if the BATT_MONITOR changes# add callback to see if the BATT_MONITOR changes
-#@vehicle.parameters.on_attribute('BATT_MONITOR')
-#def Battert_param_callback(slef, attr_name, value):
-#    print " Parameter BATT_MONITOR changed to : %s" % (attr_name,value)
+###############################################################################
+# APM26
+# Control class for interacting with the APM26 module on the Rover Sentry
+# Requirements
+# * Detects connection with APM26 over USB
+# * Supports use of the Dronekit SITL for development (optional)
+# * Creates a Dronekit vehicle object
+# * Provides Status on the APM and Dronekit connections
+# * Provides a vehicle object for the RoverSentry
+###############################################################################	
+class APM26(object):	
+# Check for APM over usb port.
+	APM_usb_VendorID=0x2341 #use specific ID for the curren APM (May change with other APM modules)
+	APM_usb_ProductID=0x0010
+	connected = False
+	found = False
+	status = "APM Not Started"
+	# Use the test SITL vehicle so we can get a verhicle refernce bdfore attempting to connect
+	# Note SITL must be started previously using dronekit-sitl rover-2.50
+	print "Looking to see if the Dronekit vehicle simulator is running (SITL) ...."
+	try:
+		vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
+	except:
+		print "SITL not present, looking for APM" 
+	# Check the APM module connection by seeing if the USB id of the APM shows up in the USB device list
+	# If it is connected then try to connect to the device using dronekit.connect()	
+	def check(self):
+		#vehicle = APM26.vehicle
+		APM26.found = False # reset check for APM each time
+		#APM_conn = APM26.APM_conn
+		VendorID = APM26.APM_usb_VendorID
+		ProductID = APM26.APM_usb_ProductID
+		#Status=APM26.Status
+		busses = usb.busses()
+		for bus in busses:
+			devices = bus.devices
+			for dev in devices:
+				if ((dev.idVendor==VendorID)and(dev.idProduct==ProductID)):
+					APM26.found = True
+					print "APM Found!"
+			if(APM26.found == False):
+				APM26.status="APM module not found: Is the APM connected via USB?"
+				APM26.connected = False   #Set the APM connnection to flase so it get started next time
+				print "%s" % APM26.status
+				return False
+			if(APM26.connected == False):
+				try:
+					print "Connecting to APM ..."
+					APM26.vehicle = connect('/dev/ttyACM0',wait_ready=True)
+					APM26.connected = True
+					APM26.status="APM Found and connected!"
+					print "Connetion Status = %s" % APM26.status
+					print "APM connected = %s" % APM26.connected
+					# Wait for vehicle to finish setup and get a GPS lock so we can set a new home location
+					if APM26.vehicle.mode.name == "INITIALISING":
+						print "Waiting for vehicle to initialise"
+						time.sleep(1)
+					#while APM26.vehicle.gps_0.fix_type < 2:
+					#	print "Waiting for GPS...:", APM26.vehicle.gps_0.fix_type
+					#	time.sleep(1)
+					# modify the home location since ArduPilot sets it to the location when GPS lock is obtained.
+					# We want the RoverSentry to return to the start location when it aborts the mission.
+					APM26.vehicle.commands.download()
+					APM26.vehicle.commands.wait_ready() # wait until download is complete.
+					time.sleep(1)
+					cmds=APM26.vehicle.commands	
+					APM26.vehicle.home_location = LocationGlobal(cmds[1].x, cmds[1].y, cmds[1].z)
+					#APM26.vehicle.commands.upload()
+					#APM26.vehicle.flush()
+					print "Initializing Home Waypoint to latitude = %s longitude = %s altitude = %s" % (cmds[1].x, cmds[1].y, cmds[1].z)
+					return True
+				except Exception as e:
+					APM26.status="APM found, but error connecting to APM: Specific Error: %s" %e.message
+					print "Error with APM connection: %s" % APM26.status
+					APM26.connected = False
+					return False
 
 #def wildcard_callback(self, attr_name, value):
 #    print "Attribute  %s = %s" % (attr_name, value)
@@ -79,89 +102,159 @@ vehicle.parameters['BATT_MONITOR']=4;
 #
 #	vehicle.add_attribute_listener('groundspeed',groundspeed_callback);
 
+
+#
+#	vehicle.add_attribute_listener('groundspeed',groundspeed_callback);
+#APM26.vehicle.add_attribute_listener('gps_0',gps_callback);
+
+#def gps_callback(self, attr_name,value):
+# 	print "GPS stuff: %s" % value;
+	
+###############################################################################
+# DroneControl
+# This class is used as the main service on the RaspberryPi for the RoverSentry
+# Requirements
+# * Provide a basic web page for control: availble at the URL:
+#    <raspberrypiaddress>:9000 on your web browser
+# * Detect connection for APM module and provide status on webpage if 
+#    theres a problem with the connection
+# * Provide status and diagnotic info during feild tests
+# * Provide basic RoverSentry control (Start , Halt, continue)
+###############################################################################	
 class DroneControl(object):
-    # Create a top level html page and supply function buttons
+	apm_module=APM26()
+	speed = 0.5
+	# Create APM connection on CherryPy startup...
+	apm_module.check()
+	# Create a top level html page and supply function buttons
 	@cherrypy.expose
 	def index(self):
-         return """<html>
+		DroneControl.apm_module.check()
+		print"module = %s and connection = %s " % (DroneControl.apm_module.found,DroneControl.apm_module.connected)
+		print"APM Status %s" % DroneControl.apm_module.status
+		if ((DroneControl.apm_module.connected == False)or(DroneControl.apm_module.found == False)):
+			msg="<html><head></head><body><H2>Error connecting to APM module!</H2>"
+			msg+="Error: %s" % APM26.status
+			msg+="<form action=\"connect\"> <button type=\"submit\"> Retry</button></form>"
+			return msg
+		else:	
+			return """<html>
 		 <head></head>
           <body>
             <H1>Rover Sentry Control Page:</H1>
-            <form action="status">
+			<BR><form action="status">
               <button type="submit">Status</button>
             </form>
-            <form action="halt">
+            <BR><form action="halt">
               <button type="submit">Halt</button>
             </form>
-            <form action="resume">
+            <BR><form action="resume">
               <button type="submit">Resume</button>
             </form>
-            <form action="home">
+            <BR><form action="home">
               <button type="submit">Home</button>
             </form>
-	    <form action="waypoints">
-	      <button type="submit">Waypoints</button>
-	    </form>
-            <form action="exit">
+			<BR><form action="waypoints">
+			  <button type="submit">Waypoints</button>
+			</form>
+            <BR><form action="exit">
               <button type="submit">Exit</button>
             </form>
           </body>
         </html>"""
-
+	###########################################################################
+	# Check to see if APM module is there and refresh the page right after
+	###########################################################################
+	@cherrypy.expose
+	def connect(self):
+		DroneControl.apm_module.check()
+		ip = socket.gethostname()
+		msg = "<html> <body> <script type=\"text/javascript\"> window.location.replace(\"http://%s:9000\");  </SCRIPT></body></html>" % ip
+		print "url=%s" % msg
+		return msg
+	###########################################################################
+	# Stop the Rover wherever it is
+	###########################################################################
 	@cherrypy.expose
 	def halt(self):
-		vehicle.mode = VehicleMode("HOLD");
-		vehicle.armed = False;
-		vehicle.groundspeed = 0;
-		vehicle.airspeed = 0;
+		DroneControl.apm_module.vehicle.mode = VehicleMode("HOLD");
+		DroneControl.apm_module.vehicle.armed = False;
+		DroneControl.apm_module.vehicle.groundspeed = 0;
+		DroneControl.apm_module.vehicle.airspeed = 0;
 		#    while vehicle.groundspeed > 0.1:
 		#	vehicle.groundspeed = 0;
 		#        	print "Waiting for groundspeed change"
 		#        	time.sleep(1)  
 		return "Vehicle Halted"
+	############################################################################	
+	# Start or Continue the mission (after a Halt)
+	############################################################################
 	@cherrypy.expose
 	def resume(self):
-		vehicle.armed = True;
-		vehicle.mode = VehicleMode("AUTO");
-		vehicle.groundspeed = speed;
-		while vehicle.groundspeed < speed:
-			vehicle.groundspeed = speed;
+		DroneControl.apm_module.vehicle.armed = True;
+		DroneControl.apm_module.vehicle.mode = VehicleMode("AUTO");
+		DroneControl.apm_module.vehicle.groundspeed = DroneControl.speed;
+		while DroneControl.apm_module.vehicle.groundspeed < DroneControl.speed:
+			DroneControl.apm_module.vehicle.groundspeed = DroneControl.speed;
+			time.sleep(1)
 			print "Resuming...";
 		return "Resume"
-
+	###########################################################################
+	# Reteive Mission (waypoints) from APM module and list on the webpage 
+	###########################################################################
 	@cherrypy.expose
 	def waypoints(self):
-		cmds = vehicle.commands;
-		cmds.download();
-		cmds.wait_ready() # wait until download is complete.
+		vehicle = APM26.vehicle
+		DroneControl.apm_module.vehicle.commands.download()
+		DroneControl.apm_module.vehicle.commands.wait_ready()
+		cmds = DroneControl.apm_module.vehicle.commands
 		wp_count = vehicle.commands.count;
-		status= " WayPoint Count: %s <BR>" % wp_count 
+		msg= "<H1>Rover Sentry Mission (Waypoints)</H1>"
+		msg+= " WayPoint Count: %s <BR>" % wp_count 
+		counter=0;
 		for waypnt in cmds:
-		   status+= "%s <BR>" % waypnt; 
-		status+= " Home Location: %s" % vehicle.home_location
-		return status
-
+			msg+= "    Waypoint %s:" % counter;
+			msg+= " : %s ," % waypnt.x; 
+			msg+= " longitude: %s " % waypnt.y;
+			msg+= " altitude: %s <BR>" % waypnt.z;
+			counter += 1
+		#APM26.vehicle.home_location = LocationGlobal(cmds[1].x, cmds[1].y, cmds[1].z)
+		msg+= " Home Location: %s <BR>" % vehicle.home_location
+		msg+= "Current Location: %s <BR>" % vehicle.location.global_frame
+		msg+= "Next Waypoint location is Waypoint %s located at lat=%s lon=%s, Alt=%s <BR>" % (cmds.next , cmds[cmds.next-1].x, cmds[cmds.next-1].y, cmds[cmds.next-1].z)
+		return msg
+	###########################################################################
+	# Return to home (Set at Waypint 1 in APM26)
+	###########################################################################
 	@cherrypy.expose
 	def home(self):
-		vehicle.armed = True;
-		vehicle.mode = VehicleMode("RTL");
-		vehicle.groundspeed = speed;
-		while vehicle.groundspeed < speed:
-			vehicle.groundspeed = speed;
+		DroneControl.apm_module.vehicle.armed = True;
+		DroneControl.apm_module.vehicle.mode = VehicleMode("RTL");
+		DroneControl.apm_module.vehicle.groundspeed = DroneControl.speed;
+		while DroneControl.apm_module.vehicle.groundspeed < DroneControl.speed:
+			DroneControl.apm_module.vehicle.groundspeed = DroneControl.speed;
 		return "Going Home....";
-
+	###########################################################################
+	# Shutdown the Dronekit Vehicle (Saves power when waiting for use)
+	# Use Resume to start up again (will take 15 seconds or more)
+	###########################################################################
 	@cherrypy.expose
 	def exit(self):
-		vehicle.close();
+		DroneControl.apm_module.vehicle.close();
+		APM26.connected = False
 		return "Closing the vehicle object"
-
+	###########################################################################
+	# List every last bit of status from Dronelit or other sources.
+	# Useful for debugging in the feild
+	###########################################################################
 	@cherrypy.expose
 	def status(self):
+		vehicle = DroneControl.apm_module.vehicle
+		DroneControl.setwaypoint(self)
 		cmds = vehicle.commands;
 		cmds.download();
 		cmds.wait_ready() # wait until download is complete.
 		wp_count = vehicle.commands.count;
-
 		#return "Armed: %s \nBattery: %s \nWaypoints: %d \nStatus: %s" % (vehicle.armed,vehicle.battery, wp_count, vehicle.system_status)
 		#return "Armed: %s \nWaypoints: %d" % (vehicle.armed, wp_count)
 		#return "Status: %s" %(vehicle.system_status)
@@ -223,6 +316,15 @@ class DroneControl(object):
 		status+= "</body></html>"
 		return status
 
+	def setwaypoint(self):
+		APM26.vehicle.commands.download()
+		APM26.vehicle.commands.wait_ready() # wait until download is complete.
+		cmds=APM26.vehicle.commands
+		APM26.vehicle.home_location = LocationGlobal(cmds[1].x, cmds[1].y, cmds[1].z)
+		APM26.vehicle.commands.upload()
+		print "Initializing Home Waypoint to latitude = %s longitude = %s altitude = %s" % (cmds[1].x, cmds[1].y, cmds[1].z)
 
+# Start the RoverSentry DronConrol CherryPy Web application
 if __name__ == '__main__':
     cherrypy.quickstart(DroneControl())
+
